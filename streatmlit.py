85c2aa8993364dd566deb52a66614ea2594b9629
DAYOUNG KIM <69188680+dayoung0805@users.noreply.github.com>
2023-05-30 21:16:45 +0900

    Update streatmlit.py

--- streatmlit.py
+++ streatmlit.py
@@ -1,46 +1,67 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach& Rocket Smoothie')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruity_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruity_list = my_fruity_list.set_index('Fruit')
 
+# Let's put al pick list here so they can pick the fruit they want to include
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruity_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruity_list.loc[fruits_selected]
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
+# display the talbe on the page
 streamlit.dataframe(fruits_to_show)
 
+# create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # write your own comment -what does the next line do? 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 
-streamlit.header('Fruityvice Fruit Advice!')
+# New Section to Display Fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruity to get information.")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+  streamlit.error()
 
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+# streamlit.text("Hello from Snowflake:")
+streamlit.header("The Fruity List Contains:")
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
+    
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+      my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "' )")
+      return "Thanks for adding " + new_fruit
+     
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
+if streamlit.button('Get Fruit List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.write(insert_row_snowflake(add_my_fruit))
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice") 
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-streamlit.dataframe(fruityvice_normalized)
-
-
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
